dc4f301766a80c828c0e5d7bd2133c6985f15d09
mihunan <mihunanv.21@uom.lk>
2025-02-19 00:10:27 -0800

    implement routing with react-router and update ProductCard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,25 @@ import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-     <Home/>
-     <Footer/>
+
+
+      <Router>
+        <div>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+          </Routes>
+        </div>
+
+      </Router>
+
+      <Home />
+      <Footer />
     </div>
   );
 }
