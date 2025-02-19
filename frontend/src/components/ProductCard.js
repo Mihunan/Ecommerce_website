83233baa49d00bc7520961538c5d416ad93a1751
mihunan <mihunanv.21@uom.lk>
2025-02-18 22:46:45 -0800

    add react-router-dom and update ProductCard

--- frontend/src/components/ProductCard.js
+++ frontend/src/components/ProductCard.js
@@ -4,7 +4,7 @@ export default function ProductCard(product) {
           <div className="card p-3 rounded">
             <img
               className="card-img-top mx-auto"
-              src="/images/products/1.jpg"
+              src= {product.image[0].url}
             />
             <div className="card-body d-flex flex-column">
               <h5 className="card-title">
