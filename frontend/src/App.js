89fd04a320cb8cd5c6a9b8c233ebac174efa60bc
mihunan <mihunanv.21@uom.lk>
2025-02-19 22:19:01 -0800

    add product detail page and search functionality; integrate react-toastify for notifications

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import Home from './pages/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ProductDetail from './pages/ProductDetail';
 
 function App() {
   return (
@@ -12,9 +13,11 @@ function App() {
 
       <Router>
         <div>
-          <Header />
+          <Header/>
           <Routes>
             <Route path="/" element={<Home />}></Route>
+            <Route path="/search" element={<Home />}></Route>
+            <Route path="/product/:id" element={<ProductDetail />}></Route>
           </Routes>
         </div>
 
